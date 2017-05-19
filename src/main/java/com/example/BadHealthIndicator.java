3b4af3ace71b3a6cd953a49387e943e2db82eb8f
Chris DeLashmutt <cdelashmutt@pivotal.io>
2017-05-19 16:33:01 -0400

    Adding bad health indicator

---
+++ src/main/java/com/example/BadHealthIndicator.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by cdelashmutt on 5/19/17.
+ */
+@Component
+public class BadHealthIndicator implements HealthIndicator {
+
+	//Just return a down status automatically
+	@Override
+	public Health health() {
+		return Health.down().build();
+	}
+}
